86c38902efadf010a9d66ba38435469db9fc9793
Murat Can <141832088+codcreater1@users.noreply.github.com>
2024-03-01 03:40:50 +0300

    Add files via upload

---
+++ classInheritance.cpp
@@ -0,0 +1,69 @@
+﻿#include "classInheritance.h"
+#include "Person.h"
+#include <time.h>
+#include "Array.h"
+
+
+int main()
+{
+    /*
+    baseClass obj1(5);
+    derivedClass obj2(8, 3);
+    derivedClass obj3(obj2);
+
+
+    obj1.print();
+    obj2.print();
+
+
+    cout << "For base; X:" << obj1.getX() << endl;
+    cout << "For derived; X:" << obj2.getX() << " Y:" << obj2.getY() << endl;
+
+    //CClass cObject;
+   */
+
+    //Person.h
+    /*
+    srand(time(NULL));
+    Student studentArray[] = { {"Ogun","Birinci",1000},{"Duygu","Keydal",1001},{"Eda","Deniz",1002,},{"Burak","Ikinci",1003} };
+    Teacher teacher1("Oguz", "Birinci", "Software");
+
+    for (int i = 0; i < 4; i++) {
+        studentArray[i].setStudentGrade(rand() % 100 + 1);
+        studentArray[i].print();
+    }
+
+    teacher1.passFailStatus(studentArray);
+    teacher1.printStudentInformation(studentArray);
+   */
+    /*
+    unsortedArray usArray(10);
+    sortedArray sArray(10);
+
+    usArray.addItem(10);
+    usArray.addItem(8);
+    usArray.addItem(12);
+    usArray.addItem(7);
+    usArray.addItem(150);
+    usArray.addItem(5);
+    usArray.removeIndexItem(0);
+    usArray.removeItem(12);
+    cout << usArray.findIndex(10) << endl;
+    usArray.printItems();
+
+    sArray.addItem(10);
+    sArray.addItem(8);
+    sArray.addItem(12);
+    sArray.addItem(7);
+    sArray.addItem(150);
+    sArray.addItem(5);
+    sArray.removeIndexItem(0);
+    sArray.removeItem(12);
+    cout<<sArray.findIndex(10)<<endl;
+    sArray.printItems();
+    */
+
+    
+
+    return 0;
+}
